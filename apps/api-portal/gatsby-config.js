15f764c0c46f7c788e00bdc636b29bcbdb0eb1f1
Brian McBride <Brian-McBride@users.noreply.github.com>
2022-02-02 05:21:40 -0800

    RapiDoc Example

---
+++ apps/api-portal/gatsby-config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  siteMetadata: {
+    title: `api-portal`,
+    description: `This is a gatsby application created by Nx.`,
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+
+    {
+      resolve: 'gatsby-plugin-svgr',
+      options: {
+        svgo: false,
+        ref: true,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: require.resolve(`@nrwl/gatsby/plugins/nx-gatsby-ext-plugin`),
+      options: {
+        path: __dirname,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `api-portal`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/logo.svg`,
+      },
+    },
+  ],
+};
---
+++ apps/api-portal/src/pages/example.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { RapiDocReact } from '@cdw/rapi-doc-react';
+
+/* eslint-disable-next-line */
+export interface ExampleProps {}
+
+export function Example(props: ExampleProps) {
+  return (
+    <RapiDocReact
+      show-header={false}
+      spec-url="https://petstore.swagger.io/v2/swagger.json"
+      render-style="read"
+      theme="dark"
+      style={{ height: '100vh', width: '100%' }}
+    />
+  );
+}
+
+export default Example;
